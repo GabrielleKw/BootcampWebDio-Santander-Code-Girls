7debbd93f6ca3e2e3e652a5b736e98588fcb7bdf
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-07-14 11:19:27 -0300

    Create Domino.java

---
+++ Desafios Básicos Java - Code Girls Santander/Domino.java
@@ -0,0 +1,15 @@
+// TODO: complete os espaços em branco com sua solução para o problema
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+import java.util.Scanner;
+
+public class DIO {
+
+	public static void main(String[] args) {
+		 Scanner leitor = new Scanner(System.in);
+      int n = leitor.nextInt();
+   System.out.println((n+1)*(n+2)/2);
+
+    }
+
+ }
