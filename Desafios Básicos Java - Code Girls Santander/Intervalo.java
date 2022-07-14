bc7a7ec3b1a6e790d2e353ae8a9bf22c753d6ed0
Gabrielle <76081229+GabrielleKw@users.noreply.github.com>
2022-07-14 11:17:02 -0300

    Create Intervalo.java

---
+++ Desafios Básicos Java - Code Girls Santander/Intervalo.java
@@ -0,0 +1,23 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+        double valor = leitor.nextDouble();
+        if (valor >= 0 && valor <= 25) {
+            System.out.println("Intervalo [0,25]");
+        } else if (valor > 25 && valor <= 50) {
+//TODO: Complete os espaços em branco com uma possível solução para o problema
+            System.out.println("Intervalo (25,50]");
+        } else if (valor > 50 && valor <= 75) {
+            System.out.println("Intervalo (50,75]");
+        } else if (valor > 75 && valor <= 100) {
+            System.out.println("Intervalo (75,100]");
+        } else {
+            System.out.println("Fora de intervalo");
+    }
+
+	}
+}
